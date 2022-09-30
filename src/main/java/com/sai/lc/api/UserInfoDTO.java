0d9a856197cebf5c0a1f57a772b0cf166958b43a
krishnaravula <krishnaravula79@gmail.com>
2022-09-30 11:41:44 -0500

    love

---
+++ src/main/java/com/sai/lc/api/UserInfoDTO.java
@@ -0,0 +1,39 @@
+package com.sai.lc.api;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class UserInfoDTO {
+
+    @NotBlank(message = "* User Name cant be blank")
+    @Size(min = 3,max = 30,message = "* you should have atleast 3-30 char")
+    private String userName;
+
+    private String crushName;
+
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getCrushName() {
+        return crushName;
+    }
+
+    public void setCrushName(String crushName) {
+        this.crushName = crushName;
+    }
+
+    @Override
+    public String toString() {
+        return "UserInfoDTO{" +
+                "userName='" + userName + '\'' +
+                ", crushName='" + crushName + '\'' +
+                '}';
+    }
+}
